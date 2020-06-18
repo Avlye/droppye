6516b8fa5d1f47b917347bcc42875f3dca5b54d7
Gabriel Almir <avlye@outlook.com>
2020-06-17 22:25:50 -0300

    Add Mongoose & Post Model

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 import routes from './routes';
 
 const app = express();
+
+/**
+ * Database Setup
+ */
+
+mongoose.connect('mongodb://localhost:27017/upload', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
